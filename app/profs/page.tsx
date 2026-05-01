31b592c4e9c161df5875404d88795e5ab86ffa3e
camara-ops <c_amara@estin.dz>
2026-05-01 15:57:45 +0100

    Create page.tsx

---
+++ app/profs/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+export default function Profs() {
+  return (
+    <div>
+      <h1>Profs</h1>
+
+      <button onClick={() => alert("Hello")}>
+        Click
+      </button>
+    </div>
+  );
+}
